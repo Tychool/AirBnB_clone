f7cdf14d0ff289167c3f45a2f674575179a13551
Hezekiahs <ok.hezekiahs@gmail.com>
2024-03-11 00:41:22 +0100

    unit test

---
+++ tests/test_models/test_amenity.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+""" Tests class Amenity """
+import unittest
+from models.amenity import Amenity
+from models.base_model import BaseModel
+import os
+import pep8
+
+
+class TestAmenity(unittest.TestCase):
+    """ Test cases for the Amenity class """
+
+    @classmethod
+    def setUpClass(cls):
+        """ Set up an instance of Amenity """
+        cls.amenity_instance = Amenity()
+        cls.amenity_instance.name = "gym"
+
+    @classmethod
+    def tearDownClass(cls):
+        """ Tear down the test environment """
+        del cls.amenity_instance
+        try:
+            os.remove("file.json")
+        except FileNotFoundError:
+            pass
+
+    def test_pep8(self):
+        """ Check if the script complies with PEP8 """
+        pep8_style_checker = pep8.StyleGuide(quiet=True)
+        pep8_errors = pep8_style_checker.check_files(['models/amenity.py'])
+        self.assertEqual(pep8_errors.total_errors, 0, "Fails PEP8")
+
+    def test_docstrings(self):
+        """ Check if methods inside Amenity class have docstrings """
+        self.assertIsNotNone(Amenity.__doc__)
+
+    def test_subclass_of_base_model(self):
+        """ Check if instance is a subclass of BaseModel """
+        self.setUpClass()
+        self.assertTrue(issubclass(self.amenity_instance.__class__, BaseModel), True)
+
+    def test_save_method(self):
+        """ Test the functionality of the save method """
+        self.setUpClass()
+        self.amenity_instance.save()
+        self.assertNotEqual(self.amenity_instance.created_at, self.amenity_instance.updated_at)
+
+if __name__ == '__main__':
+    unittest.main()
+
---
+++ tests/test_models/test_city.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+""" Tests class City """
+import models
+from models.city import City
+from models.base_model import BaseModel
+import os
+import pep8
+import unittest
+
+
+class TestCity(unittest.TestCase):
+    """ Test cases for the City class """
+
+    @classmethod
+    def setUpClass(cls):
+        """ Set up an instance of City and its attributes """
+        cls.city_instance = City()
+        cls.city_instance.name = "Oakland"
+        cls.city_instance.state_id = "California"
+
+    @classmethod
+    def tearDownClass(cls):
+        """ Tear down the test environment """
+        del cls.city_instance
+        try:
+            os.remove("file.json")
+        except FileNotFoundError:
+            pass
+
+    def test_functions(self):
+        """ Check for docstrings inside City class """
+        self.assertIsNotNone(City.__doc__)
+
+    def test_pep8(self):
+        """ Check if the script complies with PEP8 """
+        pep8_style_checker = pep8.StyleGuide(quiet=True)
+        pep8_errors = pep8_style_checker.check_files(['models/city.py'])
+        self.assertEqual(pep8_errors.total_errors, 0, "Fails PEP8")
+
+    def test_subclass_of_base_model(self):
+        """ Check if instance is a subclass of BaseModel """
+        self.setUpClass()
+        self.assertTrue(issubclass(self.city_instance.__class__, BaseModel), True)
+
+    def test_save_method(self):
+        """ Test the functionality of the save method """
+        self.setUpClass()
+        self.city_instance.save()
+        self.assertNotEqual(self.city_instance.created_at, self.city_instance.updated_at)
+
+if __name__ == '__main__':
+    unittest.main()
+
